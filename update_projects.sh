d2d27ab5b143679b0b7cd0b37d721229b48aba9a
Ed de Almeida <edvaldoajunior@gmail.com>
2017-06-01 04:29:06 -0300

    update_projects.sh now goes through a directory tree

--- update_projects.sh
+++ update_projects.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 THIS_DIR=`pwd`
-LISTA=`ls`
+LISTA=`du -a | tr "\t" " " | cut -d " " -f 2 | cut -c 2-`
 
 for i in $LISTA; do
-  dirname="$THIS_DIR/$i"
-  echo $dirname
-  cd $dirname
-  if [ -d .git ]; then
-    git pull
-  else
-    echo "Not a GIT repository..."
+  dirname="$THIS_DIR$i"
+  if [ -d $dirname ]; then
+  	cd $dirname
+  	if [ -d .git ]; then
+  	  echo "Git repository => $dirname => updating"
+  	  git pull
+  	fi
   fi
 done
+
+exit 0
